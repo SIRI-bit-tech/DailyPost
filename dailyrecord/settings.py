f5e2e64ebdffc369cd43631a9fe1258dc6752e18
SIRI-bit-tech <siritech001@gmail.com>
2025-04-04 03:02:23 +0100

    chore: add logging configuration and comment out HSTS settings in settings.py

--- dailyrecord/settings.py
+++ dailyrecord/settings.py
@@ -240,11 +240,38 @@ if not DEBUG:
     
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
      
-    # HSTS settings
-    hsts_seconds_str = os.environ.get('SECURE_HSTS_SECONDS', '31536000')
-    # Strip any comments that might be in the .env file
-    if '#' in hsts_seconds_str:
-        hsts_seconds_str = hsts_seconds_str.split('#')[0].strip()
-    SECURE_HSTS_SECONDS = int(hsts_seconds_str)  # 1 year
-    SECURE_HSTS_PRELOAD = os.environ.get('SECURE_HSTS_PRELOAD', 'True') == 'True'
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = os.environ.get('SECURE_HSTS_INCLUDE_SUBDOMAINS', 'True') == 'True'
+    # # HSTS settings
+    # hsts_seconds_str = os.environ.get('SECURE_HSTS_SECONDS', '31536000')
+    # # Strip any comments that might be in the .env file
+    # if '#' in hsts_seconds_str:
+    #     hsts_seconds_str = hsts_seconds_str.split('#')[0].strip()
+    # SECURE_HSTS_SECONDS = int(hsts_seconds_str)  # 1 year
+    # SECURE_HSTS_PRELOAD = os.environ.get('SECURE_HSTS_PRELOAD', 'True') == 'True'
+    # SECURE_HSTS_INCLUDE_SUBDOMAINS = os.environ.get('SECURE_HSTS_INCLUDE_SUBDOMAINS', 'True') == 'True'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'django.security': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
