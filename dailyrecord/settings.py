ace6ae1479fbb143b43d3005f6548deae96bcdd9
SIRI-bit-tech <siritech001@gmail.com>
2025-04-07 09:44:19 +0100

    chore: update Redis cache configuration in settings.py to use environment variable for flexibility

--- dailyrecord/settings.py
+++ dailyrecord/settings.py
@@ -282,10 +282,7 @@ LOGGING = {
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
+        'LOCATION': os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379/1'),
     }
 }
 
